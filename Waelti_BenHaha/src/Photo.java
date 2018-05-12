4b8d3aabdffa6a35e74e6a10a15aa2e07ec1a4e8
nadin <nadine.waelti@students.hevs.ch>
2018-05-13 00:06:52 +0200

    Ecran

--- Waelti_BenHaha/src/Photo.java
+++ Waelti_BenHaha/src/Photo.java
@@ -1,5 +1,23 @@
 
 public class Photo {
 String nom;
+String path;
+public Photo (String nom, String path){
+	this.nom = nom;
+	this.path = path;
+}
+
+public void ajouterPhoto (){
+	
+}
+
+public void editerPhoto (){
+	
+}
+
+public void supprimerPhoto (){
+	
+}
+
 
 }
--- Waelti_BenHaha/src/Smartphone.java
+++ Waelti_BenHaha/src/Smartphone.java
@@ -1,13 +1,12 @@
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.FlowLayout;
+import java.awt.GridLayout;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -17,13 +16,16 @@ import javax.swing.JPanel;
 
 
 public class Smartphone extends JFrame{
+	
 	private static final long serialVersionUID = 1L;
-	//Contenu de l'écran principal
+	
+	//Contenu de l'écran
 	private JButton boutonContacts = new JButton(new ImageIcon ("C:/Users/nadin/git/Waelti_BenHaha/images/Contacts.png"));
 	private JButton boutonPhotos = new JButton(new ImageIcon ("C:/Users/nadin/git/Waelti_BenHaha/images/Gallery.png"));	
 	private JButton boutonEteindre = new JButton(new ImageIcon ("C:/Users/nadin/git/Waelti_BenHaha/images/Exit.png"));	
-	private JButton boutonCalculatrice = new JButton(new ImageIcon ("C:/Users/nadin/git/Waelti_BenHaha/images/calculatrice.png"));	
-	private JButton boutonConfigurerFondEcran = new JButton(new ImageIcon ("C:/Users/nadin/git/Waelti_BenHaha/images/Settings.png"));	
+	private JButton boutonCalculatrice = new JButton(new ImageIcon ("C:/Users/nadin/git/Waelti_BenHaha/images/calculatrice.png"));
+	
+	//Bouton principal
 	private JButton launcher = new JButton(new ImageIcon ("C:/Users/nadin/git/Waelti_BenHaha/images/launcher.png"));	
 	
 	//Coque natel
@@ -31,58 +33,61 @@ public class Smartphone extends JFrame{
 	private  JLabel coqueDroitNatel = new JLabel(new ImageIcon ("C:/Users/nadin/git/Waelti_BenHaha/images/droit.png")); 
 	private  JLabel coqueHautNatel = new JLabel(new ImageIcon ("C:/Users/nadin/git/Waelti_BenHaha/images/haut.png")); 
 	private  JLabel coqueBasNatel = new JLabel(new ImageIcon ("C:/Users/nadin/git/Waelti_BenHaha/images/bas.png")); 
-	
+
+	//Panel pour l'écran
 	private JPanel panel = new JPanel();
 
 	//Constructeur 	
 	public Smartphone (){
-		
-	this.setBackground(Color.black);
-	this.setUndecorated(true);
-
-		//Coque du natel :
-		add(coqueHautNatel,BorderLayout.NORTH);
-		add(coqueBasNatel,BorderLayout.SOUTH);
-		add(coqueDroitNatel,BorderLayout.EAST);
-		add(coqueGaucheNatel,BorderLayout.WEST);
-		//Ecran au centre
-		panel.setLayout(new FlowLayout ());
-		
-		//Icones
+	
+		//Design
+		setSize(525, 900); 
+		this.setUndecorated(true);
 		add(panel, BorderLayout.CENTER);
-		
 		panel.setBackground(Color.black);
-		
-		panel.add(boutonContacts);
-		boutonContacts.addActionListener(new AfficherContacts ());
-		
-		panel.add(boutonPhotos);
-		boutonPhotos.addActionListener(new AfficherPhotos ());
-		
-		panel.add(boutonCalculatrice);
-		boutonCalculatrice.addActionListener(new AfficherCalculatrice ());
-		
-		panel.add(boutonConfigurerFondEcran);
-		boutonEteindre.addActionListener(new ConfigurerFondEcran());
-		
-		panel.add(boutonEteindre);
-		boutonEteindre.addActionListener(new EteindreSmartphone());
-		
-		
-		
-		
-		//Taille du natel :
-		setSize(525, 800); 
-		
-		//Pour que le natel s'affiche au milieu de l'ordinateur :
 		Toolkit toolkit = getToolkit();         
 		Dimension size = toolkit.getScreenSize();         
 		setLocation(size.width/2 - getWidth()/2,    size.height/2 - getHeight()/2);
 
+		//Coque du natel :
+		add(coqueHautNatel,BorderLayout.NORTH);
+		add(coqueDroitNatel,BorderLayout.EAST);
+		add(coqueGaucheNatel,BorderLayout.WEST);
+		add(coqueBasNatel,BorderLayout.SOUTH);
+		
+		//Ecran au centre
+		panel.setLayout(new GridLayout (2,2,10,10));
+		
+		//Icones		
+		panel.add(boutonContacts);
+		boutonContacts.addActionListener(new AfficherContacts ());
+		boutonContacts.setBackground(Color.black);
+		boutonContacts.setBorder(null);
+		
+		panel.add(boutonPhotos);
+		boutonPhotos.addActionListener(new AfficherPhotos ());
+		boutonPhotos.setBackground(Color.black);
+		boutonPhotos.setBorder(null);
+		
+		panel.add(boutonCalculatrice);
+		boutonCalculatrice.addActionListener(new AfficherCalculatrice ());
+		boutonCalculatrice.setBackground(Color.black);
+		boutonCalculatrice.setBorder(null);
+		
+		panel.add(boutonEteindre);
+		boutonEteindre.addActionListener(new EteindreSmartphone());
+		boutonEteindre.setBackground(Color.black);
+		boutonEteindre.setBorder(null);
+		
 	}
 	
 	//Ecouteurs :
 	
+		private Object GridLayout(int i, int j) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 		class AfficherContacts implements ActionListener{
 		@Override
 		public void actionPerformed(ActionEvent e) {
